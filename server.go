c470d774ad4affa7ea1334e10c6daa55e6c67bcf
Dmytro Sokil <dmytro.sokil@gmail.com>
2019-11-09 17:03:26 +0200

    server

--- server.go
+++ server.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
-func main()
-{
+type httpRequestHandler struct{}
+
+func (h *httpRequestHandler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
+	response := []byte("OK")
+	responseWriter.Write(response)
+}
+
+func main() {
 	var httpPort = flag.Int("http-port", 8080, "HTTP Port")
 	var profilerHTTPort = flag.Int("profiler-http-port", 6060, "Start profiler localhost")
 
@@ -14,29 +25,28 @@ func main()
 
 	// start profiler
 	go func() {
-		profilerHTTPAddress := fmt.Sprintf("localhost:%d", *profilerHTTPort)
+		profilerHTTPAddress := fmt.Sprintf("localhost:%d", (*profilerHTTPort))
 
-		log.Println("Profiler started at " + profilerHTTPAddress)
+		log.Println("Profiler started at address " + profilerHTTPAddress)
 		log.Println("Open 'http://" + profilerHTTPAddress + "/debug/pprof/' in you browser or use 'go tool pprof http://" + profilerHTTPAddress + "/debug/pprof/heap' from console")
 		log.Println("See details about pprof in https://golang.org/pkg/net/http/pprof/")
 		log.Println(http.ListenAndServe(profilerHTTPAddress, nil))
 	}()
 
-	logger := log.New(os.Stderr, "", log.LstdFlags)
-
-	httpAddress := fmt.Sprintf("localhost:%d", httpPort)
+	httpAddress := fmt.Sprintf("localhost:%d", *httpPort)
+	log.Println("HTTP server started at " + httpAddress)
 
 	httpServer := &http.Server{
 		Addr:           httpAddress,
-		Handler:        httpServerHandler,
-		ErrorLog:       logger,
+		Handler:        &httpRequestHandler{},
+		ErrorLog:       log.New(os.Stderr, "", log.LstdFlags),
 		ReadTimeout:    time.Duration(5) * time.Second,
 		WriteTimeout:   time.Duration(5) * time.Second,
 		IdleTimeout:    time.Duration(5) * time.Second,
-		MaxHeaderBytes: 1 << 20,
+		MaxHeaderBytes: 1 << 10,
 	}
 
-	err = httpServer.ListenAndServe()
+	err := httpServer.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
